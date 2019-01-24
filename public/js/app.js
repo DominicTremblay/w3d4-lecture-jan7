3549c75673e784c395397acc51e55299160dd02a
Dominic Tremblay <dominictremblay24@gmail.com>
2019-01-24 11:16:52 -0500

    removed the create elements functions

--- public/js/app.js
+++ public/js/app.js
@@ -19,10 +19,16 @@ $(document).ready(() => {
   // render a list of comments
 
   // create a single comment element from a comment obj
+  const createComment = commentObj => {
+    // format of a comment object
+    //  {
+    //    id: <this is the id of the comment>
+    //    comment: <this is the text of the comment>
+    //  }
+  };
 
   // create a single quote element
   //> renderComments
-
   const createQuoteEl = quoteObj => {
     // quoteObj has this format:
     // {
@@ -30,43 +36,6 @@ $(document).ready(() => {
     // quote: <the content of the quote
     // comments: // array of comments objects
     // }
-
-    return `<div class="card">
-          <div class="card-header" id="headingTwo">
-            <h5 class="mb-0">
-              <button class="btn btn-link collapsed" type="button" data-toggle="collapse" data-target="${
-                quoteObj.id
-              }" aria-expanded="false" aria-controls="collapseTwo">
-                ${quoteObj.quote}  
-              </button>
-            </h5>
-
-            <span>
-              <form method="get" action="/quotes/d9424e04-9df6-4b76-86cc-9069ca8ee4bb"><input type="submit" class="btn btn-secondary btn-sm" value="Edit"></form>
-              <form method="post" action="/quotes/d9424e04-9df6-4b76-86cc-9069ca8ee4bb?_method=DELETE"><input type="submit" class="btn btn-secondary btn-sm" value="Delete"></form>
-            </span>
-
-          </div>
-          <div id="d9424e04-9df6-4b76-86cc-9069ca8ee4bb" class="collapse" aria-labelledby="headingTwo" data-parent="#quote-list">
-            <div class="card-body">
-              <h4>Comments</h4>
-
-              <ul class="list-group">
-                
-                <li class="list-group-item">
-                  So awesome comment! 
-                  <span>
-                    <form method="get" action="/comments/70fcf8bd-6cb0-42f3-9887-77aa9db4f0ac/update"><input type="submit" class="btn btn-secondary btn-sm" value="Edit"></form>
-                      <form method="post" action="/comments/70fcf8bd-6cb0-42f3-9887-77aa9db4f0ac?_method=DELETE"><input type="submit" class="btn btn-secondary btn-sm" value="Delete"></form>
-                    </span>
-                </li>
-                
-              </ul>
-
-              <a href="/quotes/d9424e04-9df6-4b76-86cc-9069ca8ee4bb/comments/new" class="btn btn-info">Add Comment</a>
-            </div>
-          </div>
-        </div>`;
   };
 
   // render the list of quotes to add them to the page
@@ -82,8 +51,6 @@ $(document).ready(() => {
 
     request(options, response => {
       console.log(response);
-      const quoteEl = createQuoteEl(response[0]);
-      $('#quote-list').append(quoteEl);
     });
   };
 
